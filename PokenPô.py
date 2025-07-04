d30f12fffc43f4c8d58a6b5d21c38af601dd86b8
RODRIGO17-LIMA <rodri.batista.17@gmail.com>
2025-07-04 20:16:11 +0000

    PokenPô

--- PokenPô.py
+++ PokenPô.py
@@ -26,11 +26,11 @@ listaPokeF = ['Charizard', 'Typhlosion', 'Blaziken', 'Talonflame']      # Lista 
 listaPokeG = ['Venusaur', 'Sceptile', 'Torterra', 'Kartana']            # Lista dos pokenpôs de Grama
 listaPokeT = ['Sandslash', 'Groudon', 'Garchomp', 'Krookodile']         # Lista dos pokenpôs de Terra
 listaPokeE = ['Pikachu', 'Zapdos', 'Zekrom', 'Luxray']                  # Lista dos pokenpôs de Elétrico
-listaC = ['Água', ['Blastoise', 'Empoleon', 'Kyogre', 'Swampert'], 
-          'Fogo', ['Charizard', 'Typhlosion', 'Blaziken', 'Talonflame'], 
-          'Grama', ['Venusaur', 'Sceptile', 'Torterra', 'Kartana'],
-          'Terra', ['Sandslash', 'Groudon', 'Garchomp', 'Krookodile'],
-          'Elétrico', ['Pikachu', 'Zapdos', 'Zekrom', 'Luxray']]
+listaC = [['Blastoise', 'Empoleon', 'Kyogre', 'Swampert'], 
+          ['Charizard', 'Typhlosion', 'Blaziken', 'Talonflame'], 
+          ['Venusaur', 'Sceptile', 'Torterra', 'Kartana'],
+          ['Sandslash', 'Groudon', 'Garchomp', 'Krookodile'],
+          ['Pikachu', 'Zapdos', 'Zekrom', 'Luxray']]
 
 
 
@@ -45,7 +45,7 @@ def playGame():
     opcoes = ['1', '2', '3', '4', '5']      # Variavel usada para mostrar quais digitos o usuario deve usar
     while not validaValue(resp, opcoes):        # Verifica se a entrada do usario é compativel com a variavel opcoes
         resp = getValue(msg)    
-        
+        userElemento = resp
     sleep(1)        # Cria um intervalo de tempo antes dos próximos códigos serem ativados
     limpaTela()     # Limpa a tela(Pulando 50 linhas) para iniciar o game
     userElemento = int(resp)    # Transforma a variavel resp em um inteiro
@@ -53,7 +53,7 @@ def playGame():
     msg = '-> Digite o número de um pokenpô'    # variavel com msg que pede para o usuario digitar um numero
     resp = getValue(msg)        # Serve como entrada para o usuario
     opcoes = ['1', '2', '3', '4']       # Lista de quais numeros o usuario pode usar
-    while not validaValue(resp, opcoes):        # Valida se a resposta do usuario é compativel com as opções da lista 'opcoes'
+    while not validaValue(resp, opcoes):        # Valida se a resposta do usuario e compativel com as opções da lista 'opcoes'
         resp = getValue(msg)        # Da opção para o usario digitar novamente 
     
     userPokemon = int(resp)
@@ -77,7 +77,7 @@ def playGame():
         (userElemento == 2 and PcjogadaElemento == 5) or
         (userElemento == 3 and PcjogadaElemento == 5)):     # Verifica se vai ocerrer um empate se (Usario escolher Fogo e o Pc Elétrico, ou se o usuario escolher Grama e o Pc Elétrico, ou caso os dois escolham o mesmo elemento)
         sleep(1)        # Cria um intervalo de tempo antes dos próximos códigos serem ativados
-        mostrarCabecalho([f"{listaC[userElemento][userPokemon-1]} e {PcjogadaPokemon} se anularam", 'Empate'])       # Mostra a msg de empate
+        mostrarCabecalho([f"{listaC[userElemento-1][userPokemon-1]} e {PcjogadaPokemon} se anularam", 'Empate'])       # Mostra a msg de empate
     elif( (userElemento == 1 and PcjogadaElemento == 3) or 
          (userElemento == 1 and PcjogadaElemento == 4) or
          (userElemento == 2 and PcjogadaElemento == 3) or 
@@ -86,11 +86,11 @@ def playGame():
          (userElemento == 4 and PcjogadaElemento == 2) or 
          (userElemento == 5 and PcjogadaElemento == 1) or 
          (userElemento == 3 and PcjogadaElemento == 1)):         # Verifica se vai ocerrer uma vitoria para o jogador se (o usuario escolher Fogo e o Pc Grama, ou se escolher Grama e o Pc Terra, ou se escolher Terra e o Pc Elétrico, ou se escolher Elétrico e o Pc Água, ou se escolher Água e o Pc Fogo, ou se escolher Terra e o Pc Fogo, ou se escolher Água e o Pc Terra, ou se escolher Grama e o Pc Água)
-        mostrarCabecalho([f'{listaC[userElemento][userPokemon-1]} ganhou de {PcjogadaPokemon}', 'You WIN'])      # Mostra a msg de que o Usuario ganhou do Pc
+        mostrarCabecalho([f'{listaC[userElemento-1][userPokemon-1]} ganhou de {PcjogadaPokemon}', 'You WIN'])      # Mostra a msg de que o Usuario ganhou do Pc
           # Verifica se vai ocerrer um empate se (Usario escolher Fogo e o Pc Elétrico, ou se o usuario escolher Grama e o Pc Elétrico, ou caso os dois escolham o mesmo elemento)
     else:       # Caso o nenhuma das condições do if e elif não seja atendida o Pc vence 
        sleep(1)         # Cria um intervalo de tempo antes dos próximos códigos serem ativados
-       mostrarCabecalho([f"{PcjogadaPokemon} ganhou de {listaC[userElemento][userPokemon-1]}", 'You Lose'])     # Mostra a msg Pc ganhou do usuario
+       mostrarCabecalho([f"{PcjogadaPokemon} ganhou de {listaC[userElemento-1][userPokemon-1]}", 'You Lose'])     # Mostra a msg Pc ganhou do usuario
     
     if playAgain():     # Pergunta se o usuario quer jogar novamente
         sleep(1)        # Cria um intervalo de tempo antes dos próximos códigos serem ativados
